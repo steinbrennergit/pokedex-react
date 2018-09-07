27a9556cf2e34c749e208877feafc2a7ba591857
Twoxbradley <jimmybradley32@gmail.com>
2018-09-07 12:08:15 -0700

    redux modification

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,26 @@
 import React from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
+import {connect} from "react-redux";
 import Btns from "./pages/Btns";
+import {changeName} from "./actions";
 
-const App = () => (
-  <Router>
-    <Route exact path="/" component={Btns} />
-  </Router>
-);
+const App = (props) => {
+  return (
+  <div>
+    <h1>Hi {props.pokemon.name}!</h1>
+    <button onClick={() => props.foobar("Tucker")}>Tucker</button>
+    <button onClick={() => props.foobar("Chris")}>Chris</button>
+    <button onClick={() => props.foobar("Jimmy")}>Jimmy</button>
+  </div>
+  )
+};
 
-export default App;
+const mapStateToProps = state => ({pokemon: state.names});
+
+const mapDispatchToProps = dispatch => {
+  return { 
+    foobar: function(name) { dispatch(changeName(name)) }
+  };
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(App);
