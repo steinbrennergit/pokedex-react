301ca0fa8e6e8a9c73cf922cbdb0049ef642de07
Twoxbradley <jimmybradley32@gmail.com>
2018-09-10 11:08:56 -0700

    updating react

--- client/src/Reducers/reducer.js
+++ client/src/Reducers/reducer.js
@@ -10,3 +10,63 @@ export default (state = {
       return state
   }
 }
+
+export default (state = {
+pokemon: "button"
+}, action) => {
+  switch (action.type) {
+    case 'DISPLAY_POKEMON' :
+    return {
+      name: action.payload
+    }
+  
+    default:
+    return state
+  }
+}
+
+
+
+// function fillRows(pokemon) {
+
+//   let arrNest = [];
+
+//   for (let i = 4; i < 150; i += 5) {
+//       let arr = [pokemon[i - 4], pokemon[i - 3], pokemon[i - 2], pokemon[i - 1], pokemon[i]];
+//       arrNest.push(arr);
+//   }
+
+//   arrNest.push([pokemon[150]]);
+
+//   return (
+//       arrNest.map(el => <BtnRow key={`row-${el[0]._id}`} arr={el} />)
+//   )
+// }
+// class Btns extends Component {
+//   state = {
+//       pokemon: null
+//   }
+
+//   componentDidMount() {
+//       API.getPokemon().then((res) => {
+//           // console.log(this);
+//           this.setState({ pokemon: res.data })
+//       })
+//   }
+
+//   render() {
+//       if (this.state.pokemon) {
+//           return (
+//               <div className="flex-container">
+//                   {fillRows(this.state.pokemon)}
+//               </div>
+//           )
+//       } else {
+//           return (
+//               <div className="flex-container">
+
+//               </div>
+//           )
+//       }
+//   }
+// }
